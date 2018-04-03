e6b661694fc520eaed1917d8ccc3580cb397dae7
sunxiran <maricle01@163.com>
2018-04-03 20:55:21 +0800

    improve book module

--- src/Book.js
+++ src/Book.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react'
 
 class Book extends Component {
 
+    selectShelf(e) {
+        let value = e.target.value;
+        let book = this.props.book;
+        let oldShelf = book.shelf;
+        book.shelf = value;
+        this.props.onChangeShelf(oldShelf, book);
+    };
+
     render() {
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(' + this.props.book.imageLinks.thumbnail + ')', }}></div>
                     <div className="book-shelf-changer">
-                        <select>
+                        <select defaultValue={(this.props.book.shelf || "none")} onChange={this.selectShelf}>
                             <option value="none" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
@@ -17,8 +25,8 @@ class Book extends Component {
                         </select>
                     </div>
                 </div>
-                <div className="book-title">The Hobbit</div>
-                <div className="book-authors">J.R.R. Tolkien</div>
+                <div className="book-title">{this.props.book.title}</div>
+                <div className="book-authors">{this.props.book.authors}</div>
             </div>
         )
     }
